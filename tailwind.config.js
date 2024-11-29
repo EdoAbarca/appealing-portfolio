34ee808af52be4b769af4e605455815e9c068156
EdoAbarca <eduardo.abarca.c@usach.cl>
2024-11-29 18:26:10 -0300

    feat: Se agregan imágenes, tipo de letra a usar, scrollbar personalizado y estructura de la aplicación.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+import tailwindScrolbar from 'tailwind-scrollbar';
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'sans':['Inter', 'sans-serif']
+      }
+    },
   },
-  plugins: [],
+  plugins: [tailwindScrolbar],
 }
